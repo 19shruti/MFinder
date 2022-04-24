8f7c77d63ead99ed379060e0d58df4d0ebe91d90
SHRUTI RAJ <69633187+19shruti@users.noreply.github.com>
2022-04-24 13:25:04 +0530

    frontend

--- meal_calories.js
+++ meal_calories.js
@@ -3,33 +3,38 @@ const submit=document.getElementById('submit');
 const mealEl=document.getElementById('image');
 const resultHeading=document.getElementsByClassName('result-heading');
 const singleMeal=document.getElementById('single-meaL');
-var imagess ="";
+var imagesurl=document.getElementById('imm');
+
+var imagess =" ";
+
 
 function getdata(e)
 {
 // console.log(e);
+
 fetch(
     `https://api.spoonacular.com/recipes/${e}/information?apiKey=234df877100a4ae0b183c067f0be6ecd&includeNutrition=false`
   )
     .then((response) => response.json())
     .then((data) => {
-      imagess+=`<img src=${data.image}>`;
-      mealEl.innerHTML=imagess;
-    })
+     imagesurl= imagess+=`<img src= ${data.image}>`;  
+      mealEl.innerHTML=imagesurl;
+   })
 }
+
+
+
 //search meal parameter . 
 function searchMeal(e)
 {
     e.preventDefault();  
     //to clear the search section 
 
-    //get search meal
-    // singleMeal.innerHTML="";
-
+   //get search meal
+  // singleMeal.innerHTML="";
     console.log(search.value);
 
     const calories=search.value;
-
     if(calories)
     {
         fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=234df877100a4ae0b183c067f0be6ecd&timeFrame=day&targetCalories=${calories}`)
@@ -37,13 +42,6 @@ function searchMeal(e)
       .then((data)=>data.meals.forEach((e)=>getdata(e.id)))   
     }
 }
-
-
-
-
-
 //EVENTLISTERNERS
-
 //ADD EVENT LISTERNER FOR SUBMIT BUTTON
-
 submit.addEventListener('submit',searchMeal);
